361e5b0ba252486742b73397ad3083bbfe30f71b
Yifan Wang <namelos@outlook.com>
2016-05-02 17:27:50 +0800

    add schema

--- data/schema.js
+++ data/schema.js
@@ -1,8 +1,25 @@
 import {
   GraphQLObjectType as ObjectType,
   GraphQLSchema as Schema,
+  GraphQLNonNull as NonNull,
   GraphQLString as String,
+  GraphQLList as List
 } from 'graphql'
+
+const data = {
+  "1": {
+    "id": "1",
+    "name": "Dan"
+  },
+  "2": {
+    "id": "2",
+    "name": "Marie"
+  },
+  "3": {
+    "id": "3",
+    "name": "Jessie"
+  }
+}
 
 const GreetingsType = new ObjectType({
   name: 'Greetings',
@@ -11,13 +28,47 @@ const GreetingsType = new ObjectType({
   })
 })
 
+const userType = new ObjectType({
+  name: 'User',
+  description: 'sample user type',
+  fields: () => ({
+    id: { type: String },
+    name: { type: String }
+  })
+})
+
+const Post = new ObjectType({
+  name: 'Post',
+  description: 'This represent a Post',
+  fields: () => ({
+    _id: { type: new NonNull(String) },
+    title: { type: new NonNull(String) },
+    content: { type: String }
+  })
+})
+
 export default new Schema({
   query: new ObjectType({
     name: 'Query',
     fields: () => ({
+      user: {
+        type: userType,
+        args: {
+          id: { type: String }
+        },
+        resolve: (_, args) => data[args.id]
+      },
+      echo: {
+        type: String,
+        description: 'Echo what you enter',
+        args: {
+          message: { type: String }
+        },
+        resolve: (_, args) => `received ${args.message}`
+      },
       greetings: {
         type: GreetingsType,
-        resolve: () => ({ hello: 'Hello world!' })
+        resolve: () => ({ hello: data['2']['name'] })
       }
     })
   })
