91d5a50190964afc8955926e1f10063af2593bda
Yifan Wang <namelos@outlook.com>
2016-05-02 12:27:34 +0800

    add app

--- js/app.js
+++ js/app.js
@@ -1,15 +1,50 @@
-import 'babel-polyfill'
-
-import App from './components/App'
-import AppHomeRoute from './routes/AppHomeRoute'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Relay from 'react-relay'
+import ReactDOM from 'react-dom'
+
+let HelloApp = ({ greetings }) =>
+  <h1>{ greetings.hello }</h1>
+
+HelloApp = Relay.createContainer(HelloApp, {
+  fragments: {
+    greetings: () => Relay.QL`
+      fragment on Greetings {
+        hello,
+      }
+    `
+  }
+})
+
+class HelloRoute extends Relay.Route {
+  static routeName = 'Hello'
+  static queries = {
+    greetings: Component => Relay.QL`
+      query GreetingsQuery {
+        greetings {
+          ${Component.getFragment('greetings')}
+        }
+      }
+    `
+  }
+}
 
 ReactDOM.render(
-  <Relay.RootContainer
-    Component={App}
-    route={new AppHomeRoute()}
-  />,
+  <Relay.RootContainer Component={HelloApp} route={new HelloRoute()}/>,
   document.getElementById('root')
 )
+
+// import 'babel-polyfill'
+//
+// import App from './components/App'
+// import AppHomeRoute from './routes/AppHomeRoute'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import Relay from 'react-relay'
+//
+// ReactDOM.render(
+//   <Relay.RootContainer
+//     Component={App}
+//     route={new AppHomeRoute()}
+//   />,
+//   document.getElementById('root')
+// )
